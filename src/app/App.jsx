f31b2ad0bf02ee7526554248f0b350996decb3fa
Kosti403 <anim.myay@gmail.com>
2024-07-26 03:41:48 +0500

    fix: bild

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 import { useEffect } from "react";
 
-
 import { Routes, Route } from "react-router-dom";
 import Header from "../components/header/header";
 import Profile from "../pages/profile/profile";
@@ -10,6 +9,7 @@ import Workers from "../pages/workers/workers";
 import Statistics from "../pages/statistics/statistics";
 import Footer from "../components/footer/footer";
 import { useTelegram } from "../components/hook/useTelegram";
+import Form from "../pages/form/form";
 
 function App() {
   const { tg } = useTelegram();
@@ -24,6 +24,7 @@ function App() {
         <Route path="/" element={<Profile />} />
         <Route path="/workers" element={<Workers />} />
         <Route path="/statistics" element={<Statistics />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
       <Footer />
     </>
