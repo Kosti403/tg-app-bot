9f7a6b7578173c272da2371411b322e07b849bf8
Kosti403 <anim.myay@gmail.com>
2024-07-26 19:51:22 +0500

    fix: data in tg

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -2,23 +2,50 @@ import { useTelegram } from "../../components/hook/useTelegram";
 import "./profile.css";
 
 export default function Profile() {
-  const { userId, firstName, lastName, photoUrl } = useTelegram();
+  const { firstName, lastName, userId, photoUrl, handleClose, user } =
+    useTelegram();
 
   return (
     <div className="profile-container">
       <div className="profile-card">
         <div className="profile-header">
           <div className="profile-icon">
-            {photoUrl && (
+            {photoUrl ? (
               <img src={photoUrl} alt="Profile" className="profile-photo" />
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-user-circle"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <circle cx="12" cy="12" r="9" />
+                <circle cx="12" cy="10" r="3" />
+                <path d="M6.5 19a7 7 0 0 1 11 0" />
+              </svg>
             )}
           </div>
-          <h2 className="profile-username text-gray-600 font-semibold">Admin: {userId}!</h2>
+          <h2 className="profile-username text-gray-600 font-semibold">
+            Admin: {user}!
+          </h2>
         </div>
         <div className="profile-details text-gray-600">
-          <p className="text-gray-600">First Name: {firstName}</p>
-          <p className="text-gray-600">Last Name: {lastName}</p>
+          <p className="text-gray-600">
+            First Name: {firstName ? firstName : "Obama"}
+          </p>
+          <p className="text-gray-600">
+            Last Name: {lastName ? lastName : "Luxury"}
+          </p>
+          <p className="text-gray-600">ID: {userId ? userId : "777777"}</p>
         </div>
+        <button className="profile-close-button" onClick={handleClose}>
+          Close
+        </button>
       </div>
     </div>
   );
